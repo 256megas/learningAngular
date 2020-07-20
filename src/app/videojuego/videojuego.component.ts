8970be2a7ab329951f0b62a0727053a5e9539893
Antonio Lop <256megas@gmail.com>
2020-07-20 13:43:12 +0200

    life events

--- src/app/videojuego/videojuego.component.ts
+++ src/app/videojuego/videojuego.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'videojuego',
   templateUrl: './videojuego.component.html',
 })
-export class VideojuegoComponent{
+export class VideojuegoComponent implements OnInit,DoCheck{
   public titulo:string;
   public listado:string;
 
@@ -12,5 +12,17 @@ export class VideojuegoComponent{
     this.titulo ="Componente de videojuegs"
     this.listado="Listado de los juegos mas populares"
     console.log("se ha cargado el componente videojuego.Componente")
+  }
+
+  ngOnInit(){
+    console.log("OnInit");
+  }
+
+  ngDoCheck(){
+    console.log("DoCheck");
+  }
+
+  cambiarTitulo(){
+    this.titulo="Cambiamos titulo";
   }
 }
