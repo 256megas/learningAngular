41f42afa6cc815a07f1f2c11c0b53e594ab66b3c
Antonio Lop <256megas@gmail.com>
2020-09-03 13:37:56 +0200

    pipes and custom pipes

--- src/app/externo/externo.component.ts
+++ src/app/externo/externo.component.ts
@@ -10,15 +10,18 @@ import { PeticionesService } from '../services/peticiones.service';
 export class ExternoComponent implements OnInit {
   public user: any;
   public userId: any;
+  public fecha:any;
   constructor(private _peticionesService: PeticionesService) {
     this.userId = 1;
   }
 
   ngOnInit(): void {
     this.cargaUsuario();
+    this.fecha= new Date(2019,5,20);
   }
 
   cargaUsuario() {
+    this.user = false;
     this._peticionesService.getUser(this.userId).subscribe(
       (result) => {
         this.user = result.data;
