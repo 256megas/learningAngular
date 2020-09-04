7b0fe7485f401d05e50f76ede7ced9a959e10f14
Antonio Lop <256megas@gmail.com>
2020-09-04 10:14:15 +0200

    Forms 1

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -8,21 +8,24 @@ import { ZapatillasComponent } from './zapatillas/zapatillas.component';
 import { VideojuegoComponent } from './videojuego/videojuego.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { ExternoComponent } from './externo/externo.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 //Array de rutas
 const appRoutes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'home', component: HomeComponent },
-    { path: 'zapatillas', component: ZapatillasComponent },
-    { path: 'videojuegos', component: VideojuegoComponent },
-    { path: 'cursos', component: CursosComponent },
-    { path: 'cursos/:nombre', component: CursosComponent },
-    { path: 'cursos/:nombre/:followers', component: CursosComponent },
-    { path: 'externo', component: ExternoComponent },
-    { path: '**', component:  HomeComponent }
-  ];
-
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'zapatillas', component: ZapatillasComponent },
+  { path: 'videojuegos', component: VideojuegoComponent },
+  { path: 'cursos', component: CursosComponent },
+  { path: 'cursos/:nombre', component: CursosComponent },
+  { path: 'cursos/:nombre/:followers', component: CursosComponent },
+  { path: 'externo', component: ExternoComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: '**', component: HomeComponent },
+];
 
 //Exportar el modulo rel router
-export const appRoutingProviders:any[]=[];
-export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes)
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(
+  appRoutes
+);
